fab5266832005f142dccd92c4ccf149957813675
zKriguer <kriguerfreitas2020@gmail.com>
2022-10-05 15:37:52 -0300

    Texture/Content update

--- main.js
+++ main.js
@@ -45,11 +45,76 @@ scene.add(torus, pointLight, ambientLight);
 
 //lightHelper ajuda a visualizar de onde está vindo o ponto de luz
 const lightHelper = new THREE.PointLightHelper(pointLight);
-//gridHelper ajuda a visualizar de qual ponto de vista estamos com a camera
+
+//gridHelper ajuda a visualizar de qual ponto de vista estamos com a camera 👇
+
+/*
+{
 const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(lightHelper, gridHelper);
+}
+*/
 
+//orbit controls habilita a utilização do mouse para movimentar a camera
 const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load("/images/space.jpg");
+
+scene.background = spaceTexture;
+
+const krigTexture = new THREE.TextureLoader().load("/images/kriguer.jpeg");
+
+const krig = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshStandardMaterial({ map: krigTexture })
+);
+
+scene.add(krig);
+
+const moonTexture = new THREE.TextureLoader().load("images/moon.jpg");
+
+const normalTexture = new THREE.TextureLoader().load("images/normal.jpg");
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: normalTexture })
+);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+scene.add(moon);
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  krig.rotation.y += 0.01;
+  krig.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * 0.0002;
+  camera.positiony = t * 0.0002;
+}
+
+document.body.onscroll = moveCamera;
 
 function animate() {
   requestAnimationFrame(animate);
